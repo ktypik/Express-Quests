7573aab2ae32b6ae8665ec3e5ff8c8c00b608cee
Benjamin Laroche <benjamin.laroche28@gmail.com>
2023-05-09 17:21:23 +0200

    add-route-put-users

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.get("/api/users", movieHandlers.getUsers);
 app.get("/api/users/:id", movieHandlers.getUsersById);
 app.post("/api/movies", movieHandlers.postMovie);
 app.post("/api/users", movieHandlers.postUsers);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.put("/api/users/:id", movieHandlers.updateUsers);
 
 app.listen(port, (err) => {
   if (err) {
