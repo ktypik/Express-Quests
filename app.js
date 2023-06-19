b1e17dc0535c8637e207ecb5f9516ef6f0787bf9
Benjamin Laroche <benjamin.laroche28@gmail.com>
2023-06-19 16:50:12 +0200

    express08 done

--- app.js
+++ app.js
@@ -11,28 +11,6 @@ const port = process.env.APP_PORT ?? 5000;
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
-
-app.get("/", welcome);
-
-const movieHandlers = require("./movieHandlers");
-
-app.get("/api/movies", movieHandlers.getMovies);
-app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", movieHandlers.postMovie);
-app.put("/api/movies/:id", movieHandlers.updateMovie);
-app.delete("/api/movies/:id", movieHandlers.deleteMovie);
-
-const userHandlers = require("./userHandlers");
-
-app.get("/api/users", userHandlers.getUsers);
-app.get("/api/users/:id", userHandlers.getUserById);
-app.delete("/api/users/:id", userHandlers.deleteUser);
-
-const { hashPassword } = require("./auth.js");
-
-app.post("/api/users", hashPassword, userHandlers.postUser);
-app.put("/api/users/:id", hashPassword, userHandlers.updateUser);
-
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
@@ -40,3 +18,35 @@ app.listen(port, (err) => {
     console.log(`Server is listening on ${port}`);
   }
 });
+const movieHandlers = require("./movieHandlers");
+const { hashPassword, verifyPassword, verifyToken } = require("./auth.js");
+const userHandlers = require("./userHandlers");
+const isItDwight = (req, res) => {
+  if (
+    req.body.email === "dwight@theoffice.com" &&
+    req.body.password === "123456"
+  ) {
+    res.send("Credentials are valid");
+  } else {
+    res.sendStatus(401);
+  }
+};
+// PUBLIC
+app.get("/", welcome);
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
+app.post(
+  "/api/login",
+  userHandlers.getUserByEmailWithPasswordAndPassToNext,
+  verifyPassword
+);
+app.post("/api/users", hashPassword, userHandlers.postUser);
+// PRIVATE
+app.use(verifyToken);
+app.post("/api/movies", movieHandlers.postMovie);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+app.delete("/api/users/:id", userHandlers.deleteUser);
+app.put("/api/users/:id", hashPassword, userHandlers.updateUser);
--- userHandlers.js
+++ userHandlers.js
@@ -54,6 +54,27 @@ const getUserById = (req, res) => {
       res.status(500).send("Error retrieving data from database");
     });
 };
+// ...
+
+const getUserByEmailWithPasswordAndPassToNext = (req, res, next) => {
+  const { email } = req.body;
+
+  database
+    .query("select * from users where email = ?", [email])
+    .then(([users]) => {
+      if (users[0] != null) {
+        req.user = users[0];
+
+        next();
+      } else {
+        res.sendStatus(401);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
 
 const postUser = (req, res) => {
   const { firstname, lastname, email, city, language, hashedPassword } =
@@ -117,6 +138,7 @@ const deleteUser = (req, res) => {
 module.exports = {
   getUsers,
   getUserById,
+  getUserByEmailWithPasswordAndPassToNext,
   postUser,
   updateUser,
   deleteUser,
